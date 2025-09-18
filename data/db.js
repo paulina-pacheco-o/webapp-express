9670bb9365f9c2beb338f76145fc71d8fec08592
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-18 17:35:14 +0200

    env

--- data/db.js
+++ data/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "db_movies",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
