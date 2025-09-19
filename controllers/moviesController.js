d6d371097794931038ef682b220d9f9c4b553b27
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-19 17:06:15 +0200

    show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,7 +11,29 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-  console.log('Metodo show')
+  const { id } = req.params;
+
+  const sqlMovie = "SELECT * FROM movies WHERE id = ?";
+
+  const sqlReviews = "SELECT * FROM reviews WHERE movie_id = ?";
+
+  connection.query(sqlMovie, [id], (err, resultMovie) => {
+    if (err) return res.status(500).json({ error: `Errore nell'esecuzione della query: ${err}` })
+
+    if (resultMovie.length === 0 || resultMovie[0].id === null) return res.status(404).json({ error: `Libro non trovato` })
+
+    connection.query(sqlReviews, [id], (err, resultReview) => {
+      if (err) return res.status(500).json({ error: `Errore nell'esecuzione della query: ${err}` })
+
+      const movieWithReviews = {
+        ...resultMovie[0],
+        reviews: resultReview
+      }
+
+      res.send(movieWithReviews);
+    })
+
+  })
 }
 
 module.exports = {
