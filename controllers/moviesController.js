6007c61a1d857f9dfd46fecc67c624b5ddf657d9
paulina-pacheco-o <paulimichi05@gmail.com>
2025-09-26 17:28:10 +0200

    multer

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,8 +1,11 @@
+//importiamo la connessione al db
 const connection = require('../data/db');
 
+//index
 const index = (req, res) => {
   const sql = "SELECT * FROM movies";
 
+  //eseguo la query
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: `Errore nell'esecuzione della query: ${err}` })
 
@@ -10,21 +13,30 @@ const index = (req, res) => {
   })
 }
 
+//show
 const show = (req, res) => {
+  //recupero l'id parametro
   const { id } = req.params;
 
+  //creo la query per il libro
   const sqlMovie = "SELECT * FROM movies WHERE id = ?";
 
-  const sqlReviews = "SELECT * FROM reviews WHERE movie_id = ?";
-
+  //eseguo la query passando ora i parametri
   connection.query(sqlMovie, [id], (err, resultMovie) => {
     if (err) return res.status(500).json({ error: `Errore nell'esecuzione della query: ${err}` })
 
+    //controllo se non ho trovato il libro
     if (resultMovie.length === 0 || resultMovie[0].id === null) return res.status(404).json({ error: `Libro non trovato` })
 
+
+    //creo la query per le recensioni
+    const sqlReviews = "SELECT * FROM reviews WHERE movie_id = ?";
+
+    //eseguo la query delle reviews
     connection.query(sqlReviews, [id], (err, resultReview) => {
       if (err) return res.status(500).json({ error: `Errore nell'esecuzione della query: ${err}` })
 
+      //creo un nuovo oggetto contente i dati del libro e l'array delle recensioni
       const movieWithReviews = {
         ...resultMovie[0],
         reviews: resultReview
---
+++ middlewares/multer.js
@@ -0,0 +1,19 @@
+//importo multer
+const multer = require('multer');
+
+//definisco la cartella storage in cui effettuare l'upload
+const storage = multer.diskStorage({
+  //definisco la cartella di destinazione
+  destination: ":/public/movies_img",
+  //definisco il nome del file
+  filename: (req, file, cb) => {
+    //in questa funzione definiamo una variabile che conterrà il nome della foto in generale
+    console.log(file);
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload
